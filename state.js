eb8a148eb7e753328a9b4d4f23323d3f4cf1ba07
Alan Johnson <nalanj@fastmail.com>
2021-03-12 10:44:37 -0500

    Rework sockets binary

--- state.js
+++ state.js
@@ -9,3 +9,59 @@ function state(init, reducer) {
 }
 
 module.exports = state;
+
+export default class State {
+  constructor() {
+    this.clients = {};
+    this.subs = {};
+  }
+
+  subscribe(channel, clientId) { }
+
+  unsubscribe(channel, clientId) { }
+
+  unsubscribeSubscribedTo(channel, subscribedTo) { }
+
+  disconnectSubscribedTo(subscribedTo) {
+    const clientIds = this.subs[subscribedTo];
+
+    if (clientIds) {
+      clientIds.forEach((clientId) => {
+        if (this.clients[clientId] && this.clients[clientId].connection) {
+          this.clients[clientId].connection.close();
+        }
+      });
+    }
+  }
+
+  connect(id, client, ip, timeout) { }
+
+  removeClient(id) { }
+
+  disconnectAll() {
+    Object.values(this.clients).forEach(({ client }) => {
+      client.close();
+    });
+  }
+
+  send(channel, msgString) {
+    const clientIds = this.subs[channel];
+
+    if (clientIds) {
+      clientIds.forEach((clientId) => {
+        if (this.clients[clientId]) {
+          this.clients[clientId].client.send(msgString);
+        } else {
+          this.unsubscribe(channel, clientId);
+        }
+      });
+    }
+  }
+
+  checkSubs(clientId) {
+    if (this.clients[clientId] && this.clients[clientId].subs.length === 0) {
+      log({ event: "noSubs", clientId });
+      this.clients[clientId].client.terminate();
+    }
+  }
+}
