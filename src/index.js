370676afa20e0030b8fe1be20fad361308836c9d
Alan Johnson <nalanj@fastmail.com>
2021-07-14 18:20:34 -0400

    Add express-ws

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import express from "express";
 import log from "./log";
+import expressWs from "express-ws";
 
 const app = express();
+expressWs(app);
 
-app.get("/", (req, res) => {
-  res.send("Hello world");
+app.get("/healthz", (req, res) => {
+  res.send("OK");
+});
+
+app.ws("/client/ws", function (ws, req) {
+  ws.on("message", function (msg) {
+    console.log(msg);
+  });
+  console.log("socket", req.testing);
 });
 
 app.listen(3000, () => {
